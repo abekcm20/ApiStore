41cb7f36617861aba9ecf9a2ddd7dfe41f844278
abekcm20 <abekcm20@gmail.com>
2021-05-21 22:31:37 +0530

    Bug Fixed

--- StoreApi/StoreApi/settings.py
+++ StoreApi/StoreApi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-yok@#w#9b$zslr9(=#n7jkk@osg&%m_1j39-hbeuge6&$(xp&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-AALLOWED_HOSTS = ['127.0.0.1','apistoreabhi.herokuapp.com']
+AALLOWED_HOSTS = ['*']
 
 
 # Application definition
